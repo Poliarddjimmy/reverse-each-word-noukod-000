147e54eebf6807b796bb6ee507edbc861a874631
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-12 15:28:40 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+def reverse_each_word(string)
+ 
+  sentence1 = string.split(" ")
+  new_sentence = []
+  sentence1.each do|string|
+    new_sentence << string.reverse
+  end
+  new_sentence.join(" ")
+  
+  sentence2 = string.split(" ")
+  new_sentence = []
+  sentence2.collect do|string|
+    new_sentence << string.reverse
+  end
+  new_sentence.join(" ")
+  
+end
