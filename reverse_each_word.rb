9bcde9cc08f31d038bf5e245b5f1be5bc3ff26f3
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-12 15:31:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence1)
   
   sentence1 = sentence1.split(" ")
   new_sentence = []
-  sentence1.collect do|sentence1|
+  sentence1.collect do |sentence1|
     new_sentence << sentence1.reverse
   end
   new_sentence.join(" ")
