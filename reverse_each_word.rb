adf48baaad12cd5757631b8c0f024f97a3db94c4
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-12 15:28:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,14 +3,14 @@ def reverse_each_word(string)
   sentence1 = string.split(" ")
   new_sentence = []
   sentence1.each do|string|
-    new_sentence << string.reverse
+    new_sentence << string
   end
   new_sentence.join(" ")
   
   sentence2 = string.split(" ")
   new_sentence = []
   sentence2.collect do|string|
-    new_sentence << string.reverse
+    new_sentence << string
   end
   new_sentence.join(" ")
   
